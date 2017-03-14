ae637317d38472974a8d018c054721920125009f
Norime <loic.gondet@hotmail.fr>
2017-03-14 09:30:35 +0100

    MethodoContactLoicGondet
    Partie de Loic Gondet projet Methodo repertoire contact
    def menu

---
+++ MethodoContactLoicGondet.py
@@ -0,0 +1,25 @@
+def menu():
+    i=int(input("Que voulez vous faire? Entrez le numéro correspondent.")
+        for i in range(1,6):
+            if i=1:
+                j=int(input("Entrez un Nom ")
+                k=int(input("Entrez un prénom.")
+                l=0
+                for j in repertoire:
+                    for k in repertoire:
+                        if j==[l][0] and k==[l][1]
+                            return repertoire[l]
+                        else:
+                            l+=1
+
+    while j!=[l][0] and k!=[l][1]:
+        l+=1
+        return repertoire[l]
+
+    elif i=2:
+            for l in range(repertoire):
+                res=[]
+                if repertoire[l][5]==len(10):
+                    res+=[l]
+                print("Voici la liste de toute les personnes disposent d'un numéro de téléphone")
+                return res
